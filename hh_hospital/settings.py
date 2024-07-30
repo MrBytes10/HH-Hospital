68ee2b07304a14e6cba472d313915e22609aa375
BirkofNiki <augastinendeti@gmail.com>
2024-07-30 09:15:48 +0300

    corrected on the homepage images and fonts used

--- hh_hospital/settings.py
+++ hh_hospital/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'hh_hospital.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 #original db but changed for hosting 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
                 #'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
-}"""
+}
 
 #db made for hosting is below here
-DATABASES= {
+"""DATABASES= {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
